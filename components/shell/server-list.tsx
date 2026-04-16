67e7a3cfbafcbd0784fc977e370859fa3b70398f
nghiakk <mdm@MacBook-Pro-4.local>
2026-04-16 14:14:35 +0700

    Điểm tập kết: use animated campfire.gif (52×52) instead of SVG flame

--- components/shell/server-list.tsx
+++ components/shell/server-list.tsx
@@ -17,19 +17,25 @@ export function ServerList({
           href="/"
           title="Điểm tập kết — focus.camp"
           style={{
-            width: 48,
-            height: 48,
+            width: 56,
+            height: 56,
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
-            color: "#ff7043",
             textDecoration: "none",
           }}
         >
-          {/* Flame icon — free, no box */}
-          <svg width="32" height="32" viewBox="0 0 24 24" fill="currentColor">
-            <path d="M13.5 0.67s.74 2.65.74 4.8c0 2.06-1.35 3.73-3.41 3.73-2.07 0-3.63-1.67-3.63-3.73l.03-.36C5.21 7.51 4 10.62 4 14c0 4.42 3.58 8 8 8s8-3.58 8-8C20 8.61 17.41 3.8 13.5 0.67zM11.71 19c-1.78 0-3.22-1.4-3.22-3.14 0-1.62 1.05-2.76 2.81-3.12 1.77-.36 3.6-1.21 4.62-2.58.39 1.29.59 2.65.59 4.04 0 2.65-2.15 4.8-4.8 4.8z" />
-          </svg>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src="/campfire.gif"
+            alt="Điểm tập kết"
+            style={{
+              width: 52,
+              height: 52,
+              objectFit: "contain",
+              display: "block",
+            }}
+          />
         </Link>
       </div>
 
