27fd0b4d02525073af72ddc67472a105ba10bed2
Duong Trong Nghia <magnetnghia@gmail.com>
2026-05-13 11:38:34 +0700

    feat: hide right sidebar on settings page for more space

--- app/c/[slug]/@rightSidebar/settings/page.tsx
+++ app/c/[slug]/@rightSidebar/settings/page.tsx
@@ -1,12 +1,3 @@
-import { DefaultRightSidebar } from "../_default-sidebar";
-
-export const dynamic = "force-dynamic";
-
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ slug: string }>;
-}) {
-  const { slug } = await params;
-  return <DefaultRightSidebar slug={slug} />;
+export default function Page() {
+  return null;
 }
